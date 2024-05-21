f8290e6cde43d2a7025ca42a5eb6aaa96c5b26d9
thanattcha <158016395+thanattcha@users.noreply.github.com>
2024-05-21 21:56:24 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,160 @@
+var tourpoint = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Phu Hin Rong Kla National Park",
+        "type": "Natural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          100.9865702041862,
+          17.001388779081267
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Heart Tree House",
+        "type": "Natural",
+        "level": 4.2
+      },
+      "geometry": {
+        "coordinates": [
+          100.62562176586289,
+          16.725655788154256
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Thung Salaeng Luang National Park",
+        "type": "Natural",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.79954382401365,
+          16.69686694769436
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Namtok Chat Trakan Natural Park",
+        "type": "Natural",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.77708803372064,
+          17.27666419921853
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Chula Mani",
+        "type": "Cutural",
+        "level": 4.5
+      },
+      "geometry": {
+        "coordinates": [
+          100.21654701587153,
+          16.78790354483698
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Chan Royal Palace Historical Centre",
+        "type": "Cultural",
+        "level": 4.6
+      },
+      "geometry": {
+        "coordinates": [
+          100.26177497815411,
+          16.828598963047497
+        ],
+        "type": "Point"
+      },
+      "id": 6
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wat Klang",
+        "type": "Cultural",
+        "level": 4.7
+      },
+      "geometry": {
+        "coordinates": [
+          100.83911777511247,
+          17.103108999811255
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Phrom Phiram Public Park",
+        "type": "Special",
+        "level": 4.1
+      },
+      "geometry": {
+        "coordinates": [
+          100.16213555022892,
+          17.102137772605133
+        ],
+        "type": "Point"
+      },
+      "id": 7
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Sappraiwan Market",
+        "type": "Special",
+        "level": 4.2
+      },
+      "geometry": {
+        "coordinates": [
+          100.64798107209708,
+          16.89350668638562
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Central Phitsanulok",
+        "type": "Special",
+        "level": 4.4
+      },
+      "geometry": {
+        "coordinates": [
+          100.23306166931127,
+          16.840532834088066
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
